ee9d334b22b44f945955f29cad50ad672eb8ad5a
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-27 12:30:37 +0100

    base

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# Scroll smooth
+
+![version](https://img.shields.io/github/manifest-json/v/Natjo/scrollSmooth)
+
+
+Smooth scroll for device with no touchable screen.  
+Add momentum to the scroll.
+
+## Params
+| Keys | Type | Default | Description |
+| ------ | ------ | ------ | ------ |
+| onscroll(scrollY) | function | - | return offset of scroll |
+
+
+## Usage
+
+```html
+<div data-scroll>
+  <div data-scroll-content>
+  <!-- content -->
+  </div>
+</div>
+```
+## javascript
+
+### DOM content
+Just put the id of the popin.
+```javascript
+import ScrollSmooth from './modules/scrollSmooth';
+ScrollSmooth(scrollY => {
+	console.log(scrollY)
+});
+
+```
+
+
+## Demo
+[See codepen demo](https://codepen.io/natjo/pen/VwvxBPm?editors=0110)
---
+++ scrollSmooth.js
@@ -0,0 +1,61 @@
+/**
+ * Smooth native scroll
+ * @module ScrollSmooth
+ *
+ * @param {object} params
+ * @param {function} params.onscroll
+ * 
+ */
+
+
+function ScrollSmooth(params) {
+    const isTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    const el = document.querySelector('[data-scroll]');
+    const content = document.querySelector('[data-scroll-content]');
+   
+    if(isTouch){
+        window.addEventListener('scroll', () => onscroll(window.scrollY || window.pageYOffset), { passive: true })
+    }
+    else{
+        var raf = requestAnimationFrame || webkitRequestAnimationFrame;
+        var current = 0;
+        var last = 0;
+        var rounded = 0;
+        var ease = .09;
+
+        const scroll = () => current = window.scrollY || window.pageYOffset;
+        
+        const onresize = () => {
+            document.body.style.height = `${content.clientHeight}px`;
+            scroll();
+        }
+  
+        const run = () => {
+            last += (current - last) * ease;
+            rounded = Math.round(last * 100) / 100;
+            content.style.transform = `translate3d(0, -${rounded}px, 0)`;
+            onscroll(rounded);
+            raf(run);
+        }
+    
+        window.addEventListener('resize', onresize, {passive: true})
+        window.addEventListener('scroll', scroll, {passive: true})
+
+        onresize();
+
+        raf(run);
+
+        Object.assign(el.style, {
+            position: 'fixed',
+			overflow: 'hidden',  
+			/*top: 0,
+			left: 0,
+			height: '100%',
+			width: '100%',  */
+        })
+    }
+}
+
+
+
+export default ScrollSmooth;
